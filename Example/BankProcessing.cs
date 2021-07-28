efe0a88ea28c4490648441087f743736d3df63fa
KraussJames <james.krauss@gmail.com>
2021-07-28 10:29:40 -0400

    final tweaks

--- Example/BankProcessing.cs
+++ Example/BankProcessing.cs
@@ -11,6 +11,10 @@ namespace Example
     {
         public double Deposit(Bank bank, int accountId, double amount)
         {
+            if (amount<=0)
+            {
+                throw new ArgumentException("Deposit amount must be a positive number.");
+            }
             var acct = bank.Accounts.First(a => a.AccountId == accountId);
             acct.Balance += amount;
             //TODO: Save to Data Repository
@@ -19,22 +23,33 @@ namespace Example
 
         public double Withdrawl(Bank bank, int accountId, double amount)
         {
-            var acct = bank.Accounts.First(a => a.AccountId == accountId);
-            if (acct.Balance < amount || acct.AcctType == AccountType.Investment && acct.InvestmentAcctType == InvestmentAccountType.Individual && amount >500)
+            if (amount <= 0)
             {
-                if (acct.Balance < amount)
-                {
-                    throw new ArgumentException("Withdrawl Amount Exceeds available balance");
-                }
-                throw new ArgumentException("Individual Accounts have a withdrawl limit of $500");
+                throw new ArgumentException("Withdrawl amount must be a positive number.");
             }
-            acct.Balance -= amount;
-            //TODO: Save to Data Repository
-            return acct.Balance;
+            else
+            {
+                var acct = bank.Accounts.First(a => a.AccountId == accountId);
+                if (acct.Balance < amount || acct.AcctType == AccountType.Investment && acct.InvestmentAcctType == InvestmentAccountType.Individual && amount > 500)
+                {
+                    if (acct.Balance < amount)
+                    {
+                        throw new ArgumentException("Withdrawl Amount Exceeds available balance");
+                    }
+                    throw new ArgumentException("Individual Accounts have a withdrawl limit of $500");
+                }
+                acct.Balance -= amount;
+                //TODO: Save to Data Repository
+                return acct.Balance;
+            }
         }
 
         public Bank Transfer(Bank bank, int sourceAccountId, int targetAccountId, double amount)
         {
+            if (amount <= 0)
+            {
+                throw new ArgumentException("Transfer amount must be a positive number.");
+            }
             try
             {
                 bank.Accounts.First(a => a.AccountId == sourceAccountId).Balance -= amount;
--- UnitTests/BankUnitTests.cs
+++ UnitTests/BankUnitTests.cs
@@ -9,6 +9,12 @@ namespace UnitTests
     [TestClass]
     public class BankUnitTests
     {
+        public Example.BankProcessing bp { get; set; }
+
+        public BankUnitTests()
+        {
+            bp = new Example.BankProcessing();
+        }
         [TestMethod]
         public void TestDepositSuccess()
         {
@@ -20,9 +26,26 @@ namespace UnitTests
                 new Account() { AccountId=1,  Owner = "Adam Smith", Description = "Checking", AcctType = AccountType.Checking, Balance = 500 }
             }
             };
-            var bp = new Example.BankProcessing();
             var retVal = bp.Deposit(bank, 1, 50);
             Assert.AreEqual(retVal, 550.00);
+        }
+
+        [TestMethod]
+        public void TestDepositFailure()
+        {
+            var bank = new Bank
+            {
+                Name = "Small Bank",
+                Accounts = new List<Account>
+            {
+                new Account() { AccountId=1,
+                    Owner = "Adam Smith",
+                    Description = "Checking",
+                    AcctType = AccountType.Checking,
+                    Balance = 500 }
+            }
+            };
+            Assert.ThrowsException<ArgumentException>(() => bp.Deposit(bank, 1, -50));
         }
 
         [TestMethod]
@@ -33,10 +56,14 @@ namespace UnitTests
                 Name = "Small Bank",
                 Accounts = new List<Account>
             {
-                new Account() { AccountId=1, Owner = "Adam Smith", Description = "Checking", AcctType = AccountType.Checking, Balance = 500 }
+                new Account() {
+                        AccountId=1,
+                        Owner = "Adam Smith",
+                        Description = "Checking",
+                        AcctType = AccountType.Checking,
+                        Balance = 500 }
             }
             };
-            var bp = new Example.BankProcessing();
             var retVal = bp.Withdrawl(bank, 1, 50);
             Assert.AreEqual(retVal, 450.00);
         }
@@ -49,7 +76,11 @@ namespace UnitTests
                 Name = "Small Bank",
                 Accounts = new List<Account>
             {
-                new Account() { AccountId=1, Owner = "Adam Smith", Description = "Checking", AcctType = AccountType.Investment, Balance = 5 }
+                new Account() { AccountId=1,
+                        Owner = "Adam Smith",
+                        Description = "Checking",
+                        AcctType = AccountType.Investment,
+                        Balance = 5 }
             }
             };
             var bp = new Example.BankProcessing();
@@ -69,12 +100,26 @@ namespace UnitTests
                 new Account() { AccountId = 3, Owner = "Adam Smith", Description = "Corporate Investment", AcctType = AccountType.Investment, InvestmentAcctType = InvestmentAccountType.Corporate, Balance = 500.00 }
             }
             };
-            var bp = new Example.BankProcessing();
             var retval = bp.Transfer(bank, 1, 2, 50.50);
             Assert.AreEqual(retval.Accounts.First(a => a.AccountId == 1).Balance, 449.50);
             Assert.AreEqual(retval.Accounts.First(a => a.AccountId == 2).Balance, 550.50);
             Assert.AreEqual(retval.Accounts.First(a => a.AccountId == 3).Balance, 500.00);
         }
 
+        [TestMethod]
+        public void TestTransferFailure()
+        {
+            var bank = new Bank
+            {
+                Name = "Transfering Bank, LLC",
+                Accounts = new List<Account>
+            {
+                new Account() { AccountId = 1, Owner = "Adam Smith", Description = "Checking", AcctType = AccountType.Checking, Balance = 500.00 },
+                new Account() { AccountId = 2, Owner = "Adam Smith", Description = "Individual Investment", AcctType = AccountType.Investment, InvestmentAcctType = InvestmentAccountType.Individual, Balance = 500.00 },
+                new Account() { AccountId = 3, Owner = "Adam Smith", Description = "Corporate Investment", AcctType = AccountType.Investment, InvestmentAcctType = InvestmentAccountType.Corporate, Balance = 500.00 }
+            }
+            };
+            Assert.ThrowsException<ArgumentException>(() => bp.Transfer(bank, 1, 2, -100.50));
+        }
     }
 }
